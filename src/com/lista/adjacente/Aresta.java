1b520db67b578b5b50fe9b3aeb83217d6fee808a
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-02-03 22:02:56 -0300

    [GRAFOS] Criando lógica para algoritmo de Krukal

--- src/com/lista/adjacente/Aresta.java
+++ src/com/lista/adjacente/Aresta.java
@@ -11,6 +11,14 @@ public class Aresta implements Comparable<Aresta> {
         this.peso = peso;
     }
 
+	public Vertice getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(Vertice origem) {
+		this.origem = origem;
+	}
+
 	public Vertice getDestino() {
 		return destino;
 	}
